ca61a334606afbe8f6e43c87a19a4224214d1522
Harsh Sharma <112812978+harsharma30@users.noreply.github.com>
2024-09-12 22:58:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Start Coding/start-coding.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    void printHello() {
+        // code here
+        cout<<"Hello World";
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        Solution obj;
+        
+        obj.printHello();
+        
+    }
+}
+
+// } Driver Code Ends
