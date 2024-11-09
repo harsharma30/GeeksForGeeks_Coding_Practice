b0704a3c7ab9c23d8ea8df7d60ea85ca8ed698f8
Harsh Sharma <112812978+harsharma30@users.noreply.github.com>
2024-11-09 14:21:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum sum/minimum-sum.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String arr[] = br.readLine().split(" ");
+            int a[] = new int[arr.length];
+
+            for (int i = 0; i < arr.length; i++) {
+                a[i] = Integer.parseInt(arr[i]);
+            }
+            Solution obj = new Solution();
+            int f = 0;
+            String A = obj.minSum(a);
+            System.out.println(A);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    String minSum(int[] arr) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        StringBuilder num1 = new StringBuilder();
+        StringBuilder num2 = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                num1.append(arr[i]);
+            } else {
+                num2.append(arr[i]);
+            }
+        }
+
+        String result = addStrings(num1.toString(), num2.toString());
+        return result.replaceFirst("^0+(?!$)", ""); 
+    }
+
+    String addStrings(String num1, String num2) {
+        StringBuilder result = new StringBuilder();
+        int carry = 0, i = num1.length() - 1, j = num2.length() - 1;
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int x = i >= 0 ? num1.charAt(i--) - '0' : 0;
+            int y = j >= 0 ? num2.charAt(j--) - '0' : 0;
+            int sum = x + y + carry;
+            result.append(sum % 10);
+            carry = sum / 10;
+        }
+
+        return result.reverse().toString();
+    }
+}
