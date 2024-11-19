3bcfad2d48f16804b41faaa1d2c65c93eaeb4aab
Harsh Sharma <112812978+harsharma30@users.noreply.github.com>
2024-11-19 20:27:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Permutation/next-permutation.java
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    static void reverse(int arr[], int i, int j) {
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    
+    void nextPermutation(int[] arr) {
+        int n = arr.length, index = -1;
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] < arr[i + 1]) {
+                index = i;
+                break;
+            }
+        }
+
+
+        if (index == -1) {
+            reverse(arr, 0, n - 1);
+            return;
+        }
+
+        for (int i = n - 1; i > index; i--) {
+            if (arr[i] > arr[index]) {
+                // Swap
+                int temp = arr[i];
+                arr[i] = arr[index];
+                arr[index] = temp;
+                break;
+            }
+        }
+
+        reverse(arr, index + 1, n - 1);
+    }
+}
+
+//{ Driver Code Starts.
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine()); // Number of test cases
+        while (tc-- > 0) {
+            String[] inputLine = br.readLine().split(" ");
+            int[] arr = new int[inputLine.length];
+            for (int i = 0; i < inputLine.length; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            int N = arr.length;
+            Solution ob = new Solution();
+            ob.nextPermutation(arr); // Generate the next permutation
+            StringBuilder out = new StringBuilder();
+            for (int i = 0; i < N; i++) {
+                out.append(arr[i] + " "); // Corrected: use arr[i] instead of arr.get(i)
+            }
+            System.out.println(out.toString().trim()); // Print the output
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
