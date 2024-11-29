d0847a47b981b62e0dda4c1bd97b8727632249dd
Harsh Sharma <112812978+harsharma30@users.noreply.github.com>
2024-11-30 01:22:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Anagram/anagram.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function is to check whether two strings are anagram of each other or not.
+    bool areAnagrams(string& s1, string& s2) {
+        // Your code here
+        unordered_map<char, int> m;
+        for(auto i:s1) m[i]++;
+        for(auto i:s2) m[i]--;
+        for(auto i:m) if(i.second!=0) return false;
+        return true;
+}
+
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+
+    cin >> t;
+
+    while (t--) {
+        string c, d;
+
+        cin >> c >> d;
+        Solution obj;
+        if (obj.areAnagrams(c, d))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        cout << "~" << endl;
+    }
+}
+
+// } Driver Code Ends
