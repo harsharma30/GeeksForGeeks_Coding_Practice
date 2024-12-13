21bc59183985c8ce2dffcca0d0f114ec848aefb2
Harsh Sharma <112812978+harsharma30@users.noreply.github.com>
2024-12-13 15:57:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Learn to Comment - Java/learn-to-comment-java.java
@@ -0,0 +1,24 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        // Prints a
+        System.out.println(a);
+        // Prints b
+        //System.out.println(b);
+        // Prints c
+        System.out.println(c);
+    }
+}
+
+//{ Driver Code Starts.
+// } Driver Code Ends
